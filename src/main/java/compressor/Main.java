4cd05b83f703c5bfda8e5b1f02bd45522cd70689
Thomas <thomasjanlang.tl@gmail.com>
2025-09-18 21:45:31 +0200

    spanning functions version 1

--- src/main/java/compressor/Main.java
+++ src/main/java/compressor/Main.java
@@ -7,9 +7,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BitPacker bp= BitPackerFactory.createBitPacker("nonspanning");
+        BitPacker bp= BitPackerFactory.createBitPacker("spanning");
         int[] uncompressed={1,55,12,3,12,45,23,12};
+        System.out.println( toBinaryString(uncompressed));
         int[] compressed= bp.compress(uncompressed);
+        System.out.println( toBinaryString(compressed));
         System.out.println(Arrays.toString(compressed));
         System.out.println(bp.get(0,compressed));
         System.out.println(bp.get(1,compressed));
@@ -19,9 +21,26 @@ public class Main {
         System.out.println(bp.get(5,compressed));
         System.out.println(bp.get(6,compressed));
         System.out.println(bp.get(7,compressed));
-       
+
         int[] reuncompressed= bp.decompress(compressed);
         System.out.println(Arrays.toString(reuncompressed));
     }
+
+
+        public static String toBinaryString(int[] array) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < array.length; i++) {
+                // Konvertiert den Integer zu einem Binärstring
+                String binaryString = Integer.toBinaryString(array[i]);
+
+                // Fügt führende Nullen hinzu, um auf 32 Bits aufzufüllen
+                String paddedString = String.format("%32s", binaryString).replace(' ', '0');
+
+                sb.append(paddedString);
+                sb.append("\n");
+            }
+            return sb.toString();
+        }
+
 }
 
--- src/main/java/compressor/services/SpanningBP.java
+++ src/main/java/compressor/services/SpanningBP.java
@@ -1,14 +1,125 @@
 package compressor.services;
+
 import compressor.models.BitPacker;
 
 public class SpanningBP implements BitPacker {
-    public int[] compress(int[] array){
-        return null;
+
+    //COMPRESS  function: Input: An Array of Integers Output: An Array of Integers
+    //It compresses an array of integers to a smaller Array of Integers using bit manipulation
+    public int[] compress(int[] array) {
+
+        //The sum of all bits needed to represent all Integers of the Array
+        int bits_needed = get_number_of_bits_needed(array);
+        //The size of a chunk of data needed
+        int chunk_size = bits_needed / array.length;
+        //Size of the new array
+        int new_array_size = (int) Math.ceil((10 + bits_needed) / 32.0);
+        int[] result = new int[new_array_size];
+        //Number of unused bits
+        int nbr_unused_bit = (new_array_size * 32) - (10 + bits_needed);
+
+
+        int result_cursor = 0; //points to the current treated Integer of the array
+        int bit_cursor = 0; //points on the current bit
+        //Loop that writes the bits onto the new array
+        for (int i = 0; i < array.length; i++) {
+
+            //The writing of the chunk size and unused_bits at the beginning of the first Integer for each Compressed Array
+            if (i == 0 && bit_cursor == 0) {
+                result[result_cursor] |= chunk_size << bit_cursor;
+                result[result_cursor] |= nbr_unused_bit << 5;
+
+                bit_cursor = 10;
+
+            }
+            //Default case: Writing of the current Integer
+
+            if (bit_cursor + chunk_size > 32) {
+                int cut_point = 32 - bit_cursor;
+                int mask = (1 << cut_point) - 1;
+                int value = (array[i] >> 0) & mask;
+                result[result_cursor] |= value << bit_cursor;
+                result_cursor++;
+                bit_cursor = 0;
+                mask = (1 << (chunk_size - cut_point)) - 1;
+                value = (array[i] >> cut_point) & mask;
+                result[result_cursor] |= value << bit_cursor;
+                bit_cursor += chunk_size - cut_point;
+            } else if (bit_cursor - chunk_size == 0) {
+                result[result_cursor] |= array[i] << bit_cursor;
+                bit_cursor = 0;
+                result_cursor++;
+            } else {
+                result[result_cursor] |= array[i] << bit_cursor;
+                bit_cursor += chunk_size;
+            }
+
+        }
+
+
+        return result;
     }
-    public int[] decompress(int[] array){
-        return null;
+
+
+    public int[] decompress(int[] array) {
+        int mask = (1 << 5) - 1;
+        //Extraction of the size of a chunk of data needed
+        int chunk_size = array[0] & mask;
+        //Extraction of the number of chunks that will stay empty
+        int unused_bits = array[0] >> 5 & mask;
+        //Array size of the Array which will be returned
+        int decompressed_array_size = (array.length * 32) - 10 - unused_bits;
+        int[] result = new int[decompressed_array_size];
+
+        int cursor_array = 0;
+        int bit_cursor = 10;
+        mask = (1 << chunk_size) - 1;
+        for (int i = 0; i < decompressed_array_size; i++) {
+            //Extraction of the Integer value
+            if (32 - bit_cursor < chunk_size) {
+                int cut_point = 32 - bit_cursor;
+                int temp_value1 = array[cursor_array] >> cut_point;
+                int temp_mask = (1 << (chunk_size - cut_point)) - 1;
+                cursor_array++;
+                int temp_value2 = array[cursor_array] & temp_mask;
+                result[i] = temp_value1 << 3;
+                result[i] |= temp_value2;
+                bit_cursor = chunk_size - cut_point;
+            } else if (32 - bit_cursor == 0) {
+                bit_cursor = 0;
+                cursor_array++;
+            } else {
+                result[i] = (array[cursor_array] >> bit_cursor) & mask;
+                bit_cursor += chunk_size;
+            }
+        }
+        return result;
     }
-    public int get(int index,int[] array){
-        return 0;
+
+
+    public int get(int index, int[] array) {
+
+        int mask = (1 << 5) - 1;
+        //Extraction of the size of a chunk of data needed
+        int chunk_size = array[0] & mask;
+        //Extraction of the number of chunks that will stay empty
+        int unused_bits = array[0] >> 5 & mask;
+        //Array size to be able to check if the index is out of bounds
+        int decompressed_array_size = ((array.length * 32) ) - 10 - unused_bits;
+
+        //Test if the index is out of bounds
+        if (index < 0 || index >= decompressed_array_size) {
+            System.err.println("index out of bounds");
+            return -1;
+        }
+        int bit_cursor = 10 + chunk_size * (index );
+        //Cursor on the Integer in the compressed Array
+        int array_idex = bit_cursor / 32;
+        //Cursor on the bit in the Integer
+        int cursor = (bit_cursor % 32);
+        mask = (1 << chunk_size) - 1;
+        //Extraction of the value
+        return (array[array_idex] >> cursor) & mask;
+
     }
 }
